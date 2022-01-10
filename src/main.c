0f03d4089cb3114db0ad3c3cd794a927091bc52b
Fabbio Protopapa <fprotopapa.git@gmail.com>
2022-01-10 20:36:32 +0100

    Send sensor data every 10 sec, reduce tx power

--- src/main.c
+++ src/main.c
@@ -16,11 +16,19 @@
 
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
+#include <bluetooth/hci_vs.h>
 #include <bluetooth/conn.h>
 #include <bluetooth/uuid.h>
 #include <bluetooth/gatt.h> 
 
 #include "hts.h"
+
+// https://devzone.nordicsemi.com/f/nordic-q-a/73161/nrf-ncs---set-radio-tx-power/301671
+// https://devzone.nordicsemi.com/f/nordic-q-a/22316/openthread-nrf52840-radio-tx-power-output
+#define DEVICE_BEACON_TXPOWER_NUM  5
+static const int8_t txp[DEVICE_BEACON_TXPOWER_NUM] = {4, 2, 0, -8, -20};
+
+//static uint16_t default_conn_handle;
 
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
@@ -75,6 +83,43 @@ static struct bt_conn_auth_cb auth_cb_display = {
 	.cancel = auth_cancel,
 };
 
+// BT_HCI_VS_LL_HANDLE_TYPE_ADV             
+// BT_HCI_VS_LL_HANDLE_TYPE_CONN  
+static void set_tx_power(uint8_t handle_type, uint16_t handle, int8_t tx_pwr_lvl)
+{
+	struct bt_hci_cp_vs_write_tx_power_level *cp;
+	struct bt_hci_rp_vs_write_tx_power_level *rp;
+	struct net_buf *buf, *rsp = NULL;
+	int err;
+
+	buf = bt_hci_cmd_create(BT_HCI_OP_VS_WRITE_TX_POWER_LEVEL,
+				sizeof(*cp));
+	if (!buf) {
+		printk("Unable to allocate command buffer\n");
+		return;
+	}
+
+	cp = net_buf_add(buf, sizeof(*cp));
+	cp->handle = sys_cpu_to_le16(handle);
+	cp->handle_type = handle_type;
+	cp->tx_power_level = tx_pwr_lvl;
+
+	err = bt_hci_cmd_send_sync(BT_HCI_OP_VS_WRITE_TX_POWER_LEVEL,
+				   buf, &rsp);
+	if (err) {
+		uint8_t reason = rsp ?
+			((struct bt_hci_rp_vs_write_tx_power_level *)
+			  rsp->data)->status : 0;
+		printk("Set Tx power err: %d reason 0x%02x\n", err, reason);
+		return;
+	}
+
+	rp = (void *)rsp->data;
+	printk("Actual Tx Power: %d\n", rp->selected_tx_power);
+
+	net_buf_unref(rsp);
+}
+
 
 void main(void)
 {
@@ -89,13 +134,13 @@ void main(void)
 	bt_ready();
 
 	bt_conn_auth_cb_register(&auth_cb_display);
-
 	/* Implement indicate. At the moment there is no suitable way
 	 * of starting delayed work so we do it here
 	 */
 	while (1) {
-		k_sleep(K_SECONDS(1));
-
+		k_sleep(K_SECONDS(10));
+		set_tx_power(BT_HCI_VS_LL_HANDLE_TYPE_ADV, 0, txp[4]);
+		set_tx_power(BT_HCI_VS_LL_HANDLE_TYPE_CONN, 0, txp[4]);
 		/* Temperature measurement*/
 		hts_indicate();
 	}
